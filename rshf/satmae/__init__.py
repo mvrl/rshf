489a1f90a635c3dbef5cfcc2bdbf77fe272c2c8f
Vishu26 <s.sastry@wustl.edu>
2024-05-28 20:23:14 -0500

    satmae_models_update

--- rshf/satmae/__init__.py
+++ rshf/satmae/__init__.py
@@ -1,1 +1,3 @@
 from .model import MaskedAutoencoderViT as SatMAE
+from .model import MaskedAutoencoderGroupChannelViT as SatMAE_Pre_MS
+from .model import GroupChannelsVisionTransformer as SatMAE_Fine_MS
