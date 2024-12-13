10653b1abb65aa64e557d1b94e1ff3c078d89ee5
Vishu26 <srikumarsastry1997@gmail.com>
2024-12-13 10:49:40 +0530

    SenCLIP added

---
+++ rshf/senclip/README.md
@@ -0,0 +1,18 @@
+## SenCLIP
+
+References
+
+Arxiv: https://arxiv.org/abs/2412.08536 \
+GitHub: https://github.com/pallavijain-pj/SenCLIP/tree/main \
+Citation:
+```bib
+@misc{jain2024senclipenhancingzeroshotlanduse,
+      title={SenCLIP: Enhancing zero-shot land-use mapping for Sentinel-2 with ground-level prompting}, 
+      author={Pallavi Jain and Dino Ienco and Roberto Interdonato and Tristan Berchoux and Diego Marcos},
+      year={2024},
+      eprint={2412.08536},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV},
+      url={https://arxiv.org/abs/2412.08536}, 
+}
+```
---
+++ rshf/senclip/model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import clip
+from huggingface_hub import PyTorchModelHubMixin
+from transformers import PretrainedConfig
+import ssl
+
+#HACK: Ignore SSL errors when loading CLIP model
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class SenCLIP(nn.Module, PyTorchModelHubMixin):
+    """
+    Main class implementing the SenCLIP model with contrastive loss and pooling layers.
+
+    Args:
+        Various configurations for the model, pooling, and contrastive loss.
+    """
+    def __init__(self, config: PretrainedConfig):
+        super().__init__()
+        self.config = config
+        if type(config) is dict:
+            self.config = PretrainedConfig().from_dict(config)
+        
+        self.device = "cpu" if not torch.cuda.is_available() else self.config.device
+        self.clip, _ = clip.load(self.config.architecture, device=self.device)
+        
+
+
+
